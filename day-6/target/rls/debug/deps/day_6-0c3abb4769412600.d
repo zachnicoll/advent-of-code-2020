8530eb3ff964ac3c74af5de95e019c15242a9334
Zach Nicoll <zach.nicoll55@gmail.com>
2020-12-05 22:26:23 +1000

    Nice

--- day-6/target/rls/debug/deps/day_6-0c3abb4769412600.d
+++
@@ -1,5 +0,0 @@
-/home/zach/repos/advent-of-code-2020/day-6/target/rls/debug/deps/day_6-0c3abb4769412600.rmeta: src/main.rs
-
-/home/zach/repos/advent-of-code-2020/day-6/target/rls/debug/deps/day_6-0c3abb4769412600.d: src/main.rs
-
-src/main.rs:
